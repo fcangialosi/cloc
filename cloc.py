1c3d37363a6867f72885c274a0f3b4d4e30b168c
Frank Cangialosi <fcangialosi94@gmail.com>
2014-07-06 22:22:33 -0400

    added preliminary mode for totaling timesheets, some ranges not yet completed

--- cloc.py
+++ cloc.py
@@ -1,12 +1,34 @@
 import argparse
 import sys
 import os
-import datetime
+from datetime import datetime, timedelta, date
 import json
 
 HOME = os.path.expanduser("~")
 DATA = HOME + '/cloc.txt'
 CONFIG = HOME + '/.cloc_config'
+
+def delta_to_str(delta):
+	seconds_elapsed = int((delta.seconds % 3600) % 60)
+	minutes_elapsed = int((delta.seconds % 3600) / 60)
+	hours_elapsed = int(delta.seconds / 3600)
+	days_elapsed = int(delta.days)
+
+	result = ""
+
+	if days_elapsed > 0:
+		result+="{0} day(s), ".format(days_elapsed)
+	if hours_elapsed > 0:
+		result+="{0} hour(s), ".format(hours_elapsed)
+	if minutes_elapsed > 0:
+		result+="{0} minutes(s), ".format(minutes_elapsed)
+	if seconds_elapsed > 0:
+		result+="{0} seconds(s)".format(seconds_elapsed)
+
+	return result
+
+def str_to_date(string):
+	return datetime.strptime(string, "%Y-%m-%d %X")
 
 def write(time, action, project, msg):
 	with open(DATA, 'a') as f:
@@ -17,7 +39,7 @@ def write(time, action, project, msg):
 		f.write("\n")
 
 def cloc_in(args,msg):
-	now = datetime.datetime.now()
+	now = datetime.now()
 	now_str = str(now).split(".")[0]
 
 	project = "None"
@@ -29,7 +51,7 @@ def cloc_in(args,msg):
 	print "You cloc'd in at " + str(now_str)
 
 def cloc_out(args,msg):
-	now = datetime.datetime.now()
+	now = datetime.now()
 	now_str = str(now).split(".")[0]
 
 	project = "None"
@@ -48,23 +70,80 @@ def cloc_out(args,msg):
 
 	write(now_str, 'out', project, msg)
 
-	datetime_in = datetime.datetime.strptime(time_in, "%Y-%m-%d %X")
+	datetime_in = datetime.strptime(time_in, "%Y-%m-%d %X")
 	time_elapsed = now - datetime_in
-	seconds_elapsed = int((time_elapsed.seconds % 3600) % 60)
-	minutes_elapsed = int((time_elapsed.seconds % 3600) / 60)
-	hours_elapsed = int(time_elapsed.seconds / 3600)
-	days_elapsed = int(time_elapsed.days)
-
+	
 	print "You cloc'd out at " + str(now_str)
 	print "You worked for",
-	if days_elapsed > 0:
-		print "{0} day(s),".format(days_elapsed),
-	if hours_elapsed > 0:
-		print "{0} hour(s),".format(hours_elapsed),
-	if minutes_elapsed > 0:
-		print "{0} minutes(s),".format(minutes_elapsed),
-	if seconds_elapsed > 0:
-		print "{0} seconds(s)".format(seconds_elapsed)
+	print delta_to_str(time_elapsed)
+	
+def total_range(range_start, range_end):
+
+	total = timedelta(microseconds=0)
+
+	with open(DATA, 'r') as f:
+
+		r = f.readlines()
+		periods = zip(*[iter(r)] * 2)
+
+		for period in periods:
+
+			period_start = str_to_date(period[0].split()[0] + " " + period[0].split()[1])
+			period_end = str_to_date(period[1].split()[0] + " " + period[1].split()[1])
+			period_start_in_range = (range_start <= period_start <= range_end)
+			period_end_in_range = (range_start <= period_end <= range_end)
+
+			if(period_start_in_range and period_end_in_range):
+				total += (period_end-period_start)
+			elif(period_start_in_range):
+				total += (range_end-period_start)
+			elif(period_end_in_range):
+				total += (period_end-range_start)
+
+	return total
+
+def cloc_total(args):
+	if len(args) < 2:
+		when = 'today'
+	else:
+		when = args[1]
+
+	if when == 'today':
+		today = date.today()
+		today_start = datetime.combine(today, datetime.min.time())
+		today_end = datetime.combine(today, datetime.max.time())
+		total = total_range(today_start, today_end)
+
+	elif when == 'yesterday':
+		yesterday = date.today() - timedelta(days=1)
+		yesterday_start = datetime.combine(yesterday, datetime.min.time())
+		yesterday_end = datetime.combine(yesterday, datetime.max.time())
+		total = total_range(yesterday_start, yesterday_end)
+
+	elif when == 'week':
+		print 'TODO week'
+
+	elif when == 'month':
+		print 'TODO month'
+
+	elif when == 'year':
+		print 'TODO year'
+
+	else:
+		when = ' '.join(args[1:])
+		this_year = str(date.today().year)
+		if len(when.split('to')) <= 1:
+			start_day = end_day = datetime.strptime(when.strip() + " " + this_year, "%b %d %Y").date()
+		else:
+			start_day = datetime.strptime(when.split('to')[0].strip()+ " " + this_year , "%b %d %Y").date()
+			end_day = datetime.strptime(when.split('to')[1].strip() + " " + this_year, "%b %d %Y").date()
+
+		query_start = datetime.combine(start_day, datetime.min.time())
+		query_end = datetime.combine(end_day, datetime.max.time())
+		total = total_range(query_start, query_end)
+
+	print delta_to_str(total)
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(prog='cloc', description='cloc is tiny command line application to help you keep track of when you work.')
@@ -108,6 +187,8 @@ if __name__ == "__main__":
 		cloc_in(args['action'],msg)
 	elif(args['action'][0] == 'out'):
 		cloc_out(args['action'],msg)
+	elif(args['action'][0] == 'total'):
+		cloc_total(args['action'])
 	else:
 		print "Sorry, {0} is not a valid mode"
 		sys.exit(1)
